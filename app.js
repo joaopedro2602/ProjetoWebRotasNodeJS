67b265b829bcb7d1c2c12dc73480a98e75576e4c
joaopedro2602 <joao.cabral14@etec.sp.gov.br>
2024-03-23 21:44:14 -0300

    v1

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require("express")
+const app = express()
+const handlebars = require("express-handlebars").engine
+
+app.engine("handlebars", handlebars({defaultLayout: "main"}))
+app.set("view engine", "handlebars")
+
+app.get("/",function(req, res){
+    res.render("index")
+})
+
+app.get("/consulta",function(req, res){
+    res.render("consulta")
+})
+
+app.listen(8081, function(){
+    console.log("Servidor ativo")
+})

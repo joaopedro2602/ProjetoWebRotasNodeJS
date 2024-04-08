17c296457c9a560bd93f461c048d36a08069d14f
João Pedro Cabral <99431017+joaopedro2602@users.noreply.github.com>
2024-04-07 22:26:32 -0300

    Update app.js

--- app.js
+++ app.js
@@ -13,6 +13,11 @@ app.get("/consulta",function(req, res){
     res.render("consulta")
 })
 
+app.get("/editar",function(req, res){
+    res.render("editar")
+})
+
+
 app.listen(8081, function(){
     console.log("Servidor ativo")
 })
